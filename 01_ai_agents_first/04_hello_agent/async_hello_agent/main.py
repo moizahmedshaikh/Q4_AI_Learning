eeb60e31a1803f2787fa8f4039541085c52992d5
Moiz Ahmed <moizaman905@gmail.com>
2025-06-19 14:46:12 +0500

    updated

--- 01_ai_agents_first/04_hello_agent/async_hello_agent/main.py
+++ 01_ai_agents_first/04_hello_agent/async_hello_agent/main.py
@@ -1,3 +1,5 @@
+
+
 import os
 from dotenv import load_dotenv
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
@@ -8,35 +10,35 @@ load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-if not gemini_api_key:
-    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
-
-external_client = AsyncOpenAI(
+client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
 model = OpenAIChatCompletionsModel(
     model="gemini-2.0-flash",
-    openai_client=external_client
+    openai_client=client
 )
 
 config = RunConfig(
     model=model,
-    model_provider=external_client,
+    model_provider=client,
     tracing_disabled=True
 )
 
-
 async def main():
+
     agent = Agent(
-        name="Assistant",
-        instructions="You are helpful Assistent.",
-        model=model
+        name= "English Teacher",
+        instructions= "you are a best English Teacher in the world",
+        model= model
     )
 
-    result = await Runner.run(agent, "Tell me about recursion in programming.", run_config=config)
+    prompt = input("how can i Help you? ")
+
+    result = await Runner.run(agent, prompt, run_config= config)
     print(result.final_output)
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
